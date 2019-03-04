cea1b6cf1f51c680e01b704defa52ce6f66040b2
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-04 09:28:57 +0100

    Connvert gradle from groovy to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,136 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import no.nils.wsdl2java.Wsdl2JavaTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "no.nav.syfo"
+version = "1.0.0"
+
+val artemisVersion = "2.6.2"
+val coroutinesVersion = "0.23.4"
+val fellesformatVersion = "1.0"
+val ibmMqVersion = "9.1.0.0"
+val javaxActivationVersion = "1.1.1"
+val jacksonVersion = "2.9.7"
+val jaxbApiVersion = "2.4.0-b180830.0359"
+val jaxbVersion = "2.3.0.1"
+val kafkaVersion = "2.0.0"
+val kafkaEmbeddedVersion = "1.0.0"
+val kluentVersion = "1.39"
+val ktorVersion = "1.1.3"
+val logbackVersion = "1.2.3"
+val logstashEncoderVersion = "5.1"
+val prometheusVersion = "0.5.0"
+val spekVersion = "2.0.0-rc.1"
+val jaxwsApiVersion = "2.3.1"
+val confluentVersion = "4.1.1"
+val jaxbBasicAntVersion = "1.11.1"
+val javaxAnnotationApiVersion = "1.3.2"
+val jaxwsToolsVersion = "2.3.1"
+val jaxbRuntimeVersion = "2.4.0-b180830.0438"
+val arenaSykemdlingVersion = "1.0.3-SNAPSHOT"
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
+}
+
+
+plugins {
+    java
+    id("no.nils.wsdl2java") version "0.10"
+    kotlin("jvm") version "1.3.21"
+    id("org.jmailen.kotlinter") version "1.21.0"
+    id("com.diffplug.gradle.spotless") version "3.14.0"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+}
+
+
+buildscript {
+    dependencies {
+        classpath("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+        classpath("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
+        classpath("com.sun.activation:javax.activation:1.2.0")
+    }
+}
+
+repositories {
+    maven (url= "https://repo.adeo.no/repository/maven-snapshots/")
+    maven (url= "https://repo.adeo.no/repository/maven-releases/")
+    maven (url= "https://dl.bintray.com/kotlin/ktor")
+    maven (url= "https://dl.bintray.com/spekframework/spek-dev")
+    maven (url= "http://packages.confluent.io/maven/")
+    maven (url= "https://kotlin.bintray.com/kotlinx")
+    mavenCentral()
+    jcenter()
+}
+
+val navWsdl= configurations.create("navWsdl") {
+    setTransitive(false)
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation ("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    implementation ("io.prometheus:simpleclient_common:$prometheusVersion")
+
+    implementation ("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation ("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation ("io.ktor:ktor-client-auth-basic:$ktorVersion")
+    implementation ("io.ktor:ktor-client-jackson:$ktorVersion")
+
+    implementation ("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation ("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation ("com.ibm.mq:com.ibm.mq.allclient:$ibmMqVersion")
+
+    implementation ("org.apache.kafka:kafka_2.12:$kafkaVersion")
+    implementation ("org.apache.kafka:kafka-streams:$kafkaVersion")
+
+    implementation ("com.fasterxml.jackson.module:jackson-module-jaxb-annotations:$jacksonVersion")
+    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    implementation ("no.nav.helse.xml:arenaSykmelding-1:$arenaSykemdlingVersion")
+
+    implementation ("javax.xml.bind:jaxb-api:$jaxbApiVersion")
+    implementation ("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
+    implementation ("javax.activation:activation:$javaxActivationVersion")
+
+    testImplementation ("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation ("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
+        exclude(group = "org.eclipse.jetty") // conflicts with WireMock
+    }
+    testImplementation ("no.nav:kafka-embedded-env:$kafkaEmbeddedVersion")
+    testImplementation ("org.apache.activemq:artemis-server:$artemisVersion")
+    testImplementation ("org.apache.activemq:artemis-jms-client:$artemisVersion")
+
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testRuntimeOnly ("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+
+}
+
+
+tasks {
+    create("printVersion") {
+        println(project.version)
+    }
+
+    withType<ShadowJar> {
+        transform(ServiceFileTransformer::class.java)
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+        testLogging.events("passed", "skipped", "failed")
+    }
+}
